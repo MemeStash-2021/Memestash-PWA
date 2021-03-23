274826a4680275b9aef4e3282f6cb5f441cc6716
Bo <bo.robbrecht@student.howest.be>
2021-03-23 21:25:59 +0100

    Able to install apllication, but offline capability is broken.

--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,25 @@
 "use strict";
+const CACHE_NAME = "MemeDex-V1"
+const CACHE_URLS = [
+    "index.html",
+    "collection.html",
+    "card.html",
+    "assets/css/index.css",
+    "assets/css/collection.css",
+    "assets/css/card.css"
+]
 
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function () {
-        navigator.serviceWorker.register('./sw.js').then(function (registration) {
-            // Registration was successful
-            console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        }, function (err) {
-            // registration failed :(
-            console.log('ServiceWorker registration failed: ', err);
-        });
-    });
-}
+self.addEventListener("install", function (e) {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(CACHE_URLS);
+        })
+    )
+});
+
+self.addEventListener("fetch", function (e) {
+    e.respondWith(fetch(e.request)
+        .catch(() => caches.open(CACHE_NAME).then(cache => {
+            return cache.match(e.request);
+        })));
+});
