7390fc47331a27d690c514e86409ac05b93e62f0
Bo <bo.robbrecht@student.howest.be>
2021-03-26 16:07:26 +0100

    Able to dynamically load the cards

--- public/assets/js/collection.js
+++ public/assets/js/collection.js
@@ -1,19 +1,31 @@
-import {get} from "./config/config.js";
 "use strict";
+import {get} from "./config/config.js";
+
 
 document.addEventListener("DOMContentLoaded", scriptLoader);
 
 function scriptLoader() {
-    fetchCards()
+    fetchCards().then((result) => displayCards(result))
 }
 
-function fetchCards() {
+
+
+function displayCards(data) {
+    data.cards.forEach(card => {
+        document.querySelector("main").innerHTML +=
+            `<figure class="card">
+            <img src="${card.image}" alt="${card.name} image">
+            <figcaption>
+                <p><em class="fas fa-eye"></em>22K</p>
+                <p><em class="fas fa-heart"></em>10K</p>
+            </figcaption>`
+        }
+    )
+}
+
+async function fetchCards() {
     const userId = (localStorage.getItem("userId") === null) ? 1 : localStorage.getItem("userId")
-    get(`/users/${userId}/cards`).then((result) => {
-        console.log(result)
+    return await get(`/users/${userId}/cards`).then((result) => {
+        return result
     })
-}
-
-function displayCards(cards) {
-    //TODO: display cards
 }
