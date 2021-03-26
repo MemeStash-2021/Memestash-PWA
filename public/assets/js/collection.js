63449fc80f610c520bb03371b6868e020b85a4e5
Bo <bo.robbrecht@student.howest.be>
2021-03-26 11:16:57 +0100

    Added base files

---
+++ public/assets/js/collection.js
@@ -0,0 +1,17 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", scriptLoader);
+
+
+
+function scriptLoader() {
+    fetchCards().then((cards) => displayCards(cards))
+}
+
+function fetchCards() {
+    //TODO: fetch cards
+}
+
+function displayCards(cards) {
+    //TODO: display cards
+}
