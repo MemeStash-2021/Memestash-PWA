d1dc1450480667ea679d1f517f6851dc31670040
Bo <bo.robbrecht@student.howest.be>
2021-04-24 15:45:59 +0200

    Able to display the card's info

---
+++ public/assets/js/card.js
@@ -0,0 +1,19 @@
+"use strict";
+import {get} from "./config/config.js";
+import { constructCard } from "./util/wrappers.js"
+
+document.addEventListener("DOMContentLoaded", scriptLoader);
+
+function scriptLoader() {
+    displayCard()
+}
+
+function displayCard() {
+    let cardId = localStorage.getItem('cardId')
+    get(`/cards?id=${cardId}`).then(card => {
+        document.querySelector("main").innerHTML += constructCard(card[0])
+        document.querySelector("main").innerHTML += `
+        <h2>${card[0].name}</h2>
+        <p>${card[0].description}</p>`
+    })
+}
--- public/assets/js/util/wrappers.js
+++ public/assets/js/util/wrappers.js
@@ -2,7 +2,7 @@ import { formatNumbers } from "./formatter.js"
 
 export function constructCard(data) {
     return `<figure class="card" data-id="${data.id}">
-        <img src="${data.image}" alt="${data.name} image" data-id="${data.id}">
+        <img src="${(data.image === undefined) ? data.picture : data.image}" alt="${data.name} image" data-id="${data.id}">
             <figcaption data-id="${data.id}">
                 <p data-id="${data.id}"><em class="fas fa-eye" data-id="${data.id}"></em>${formatNumbers(data.views)}</p>
                 <p data-id="${data.id}"><em class="fas fa-heart" data-id="${data.id}"></em>${formatNumbers(data.likes)}</p>
