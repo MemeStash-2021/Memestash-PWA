f765dde850f3bc9d19ff7fda81f4a745508743aa
Bo <bo.robbrecht@student.howest.be>
2021-04-24 14:52:30 +0200

    Made cards clickable

---
+++ public/assets/js/util/clickable.js
@@ -0,0 +1,4 @@
+export function redirectCard(e) {
+    e.preventDefault()
+    console.log(e)
+}
--- public/assets/js/util/wrappers.js
+++ public/assets/js/util/wrappers.js
@@ -3,6 +3,7 @@ import { formatNumbers } from "./formatter.js"
 export function constructCard(data) {
     return `<figure class="card">
         <img src="${data.image}" alt="${data.name} image">
+        <p hidden class="card-id">${data.id}</p>
             <figcaption>
                 <p><em class="fas fa-eye"></em>${formatNumbers(data.views)}</p>
                 <p><em class="fas fa-heart"></em>${formatNumbers(data.likes)}</p>
