c527d5eae2905b1e94ce2fe467054c7b7cbb82a1
Bo <bo.robbrecht@student.howest.be>
2021-05-22 11:32:22 +0200

    Able to notify the user if he doesn't have enough coins

--- public/assets/js/api.js
+++ public/assets/js/api.js
@@ -1,19 +1,42 @@
 async function get(uri) {
-    return fetch(`${apiURL}${uri}`, {
-        method: "GET"
-    }).then((response) => {
-        return response.json()
+    return new Promise((resolve, reject) => {
+        fetch(`${apiURL}${uri}`, {
+            method: "GET"
+        }).then((response) => {
+            if (!response.ok) {
+                reject(HTTPError(response))
+            }
+            resolve(response.json())
+        })
     })
 }
 
 async function put(uri, body = null) {
-    return fetch(`${apiURL}${uri}`, {
-        method: "PUT",
-        body: (body == null) ? null : body,
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    }).then((response) => {
-        return response.json()
-    })
+    return new Promise(((resolve, reject) => {
+        fetch(`${apiURL}${uri}`, {
+            method: "PUT",
+            body: (body == null) ? null : body,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then((response) => {
+            if (!response.ok) {
+                reject(HTTPError(response))
+            }
+            resolve(response.json())
+        })
+    }))
+}
+
+function HTTPError(response) {
+    let error
+    switch (response.status){
+        case 403: error = new Error("You don't have enough coins"); break;
+        case 404: error = new Error("We couldn't find the requested entity"); break;
+        case 409: error = new Error("The username is already taken"); break;
+        default: error = new Error("Internal server error"); break;
+    }
+    error.response = response
+    error.status = response.status
+    return error
 }
--- public/assets/js/card.js
+++ public/assets/js/card.js
@@ -5,30 +5,39 @@ let cardId
 
 function scriptLoader() {
     document.querySelector(".popup").style.display = "none"
+    document.querySelector(".error").style.display = "none"
     displayCard()
 }
 
 function displayCard() {
     cardId = localStorage.getItem('cardId')
-    get(`/cards?id=${cardId}`).then(card => {
-        document.querySelector("main").innerHTML += constructCard(card[0])
-        document.querySelector("main").innerHTML += `
+    get(`/cards?id=${cardId}`)
+        .then(card => {
+            document.querySelector("main").innerHTML += constructCard(card[0])
+            document.querySelector("main").innerHTML += `
         <h2>${card[0].name}</h2>
         <p>${card[0].description}</p>`
-        document.querySelector("footer").innerHTML += `
+            document.querySelector("footer").innerHTML += `
         <a href="index.html"><em class="fas fa-arrow-left"></em></a>
         <p><em class="fas fa-coins"></em>${card[0].price} Buy</p>`
-        document.querySelector("footer p").addEventListener("click", buyCard)
-    })
+            document.querySelector("footer p").addEventListener("click", buyCard)
+        })
 }
 
 function buyCard() {
-    put(`/users/${userId}/cards/${cardId}`).then(response => {
-        let newCard = response.cards.pop()
-        document.querySelector(".popup").style.display = "block"
-        document.querySelector(".popup").innerHTML = `
+    put(`/users/${userId}/cards/${cardId}`)
+        .then(response => {
+            let newCard = response.cards.pop()
+            document.querySelector(".popup").style.display = "block"
+            document.querySelector(".popup").innerHTML = `
         <h2> Card bought!</h2>
         <p> A "${newCard.name}" has been bought and added to your inventory</p>`
-        setTimeout(() => document.querySelector(".popup").style.display = "none", 5000)
-    })
+            setTimeout(() => document.querySelector(".popup").style.display = "none", 5000)
+        })
+        .catch(err => {
+            document.querySelector(".error").style.display = "block"
+            document.querySelector(".error").innerHTML = `
+        <h2>Something went wrong!</h2>
+        <p>${err.message}</p>`
+        })
 }
